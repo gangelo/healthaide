c60b6696ed0929a2326a2a80e4c14ae89a6693be
gangelo <web.gma@gmail.com>
2025-02-27 16:06:00 -0500

    Add a user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,59 @@
+# frozen_string_literal: true
+
+# The user model for this application.
 class User < ApplicationRecord
+  before_save :downcase_email
+
+  # Include default devise modules. Others available are: :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable, :trackable,
+         authentication_keys: [ :email_or_username ]
+
+  attr_accessor :email_or_username
+
+  validates :first_name, presence: true, length: { maximum: 64 }
+  validates :last_name, presence: true, length: { maximum: 64 }
+
+  # Should be the same as the email regex in config/initializers/devise.rb
+  validates :email, length: { minimum: 3, maximum: 320 }, unless: -> { email.blank? }
+
+  validates :username, presence: true, if: -> { username.blank? }
+  validates :username, length: { minimum: 4, maximum: 64 }, unless: -> { username.blank? }
+  validates :username, uniqueness: { case_sensitive: false }, unless: -> { username.blank? }
+
+  validate :password_complexity, if: -> { password.present? }
+
+  class << self
+    def find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if (email_or_username = conditions.delete(:email_or_username))
+        where(conditions.to_hash).where([ "lower(username) = :value OR lower(email) = :value",
+                                         { value: email_or_username.downcase } ]).first
+      # NOTE: We can tell Devise to allow case-insensitive username authentication using
+      # `config.case_insensitive_keys = [:email, :username]` in the config/initializers/devise.rb
+      # initializer. However, doing so would cause Devise to downcase the username whenever a
+      # user is created or updated. We do not want to do this because we want to preserve the
+      # case of the username the user entered. Therefore, we have to manually downcase the
+      # username here to allow case-insensitive username authentication.
+      elsif (username = conditions.delete(:username))
+        where(conditions.to_hash).where([ "lower(username) = :value", { value: username.downcase } ]).first
+      else
+        # NOTE: case-insensitive email authentication is already handled by Devise here.
+        where(conditions.to_hash).first
+      end
+    end
+  end
+
+  private
+
+  def password_complexity
+    return if password.match?(/\A.*(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).*\z/)
+
+    errors.add(:password, t("errors.messages.password_complexity"))
+  end
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "index/home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,6 +8,8 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  devise_for :users
 
   # Defines the root path route ("/")
   root to: "home#index"
