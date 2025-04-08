66c051c97ec8b11418deabe1f9d0db2d2ab0ee24
gangelo <web.gma@gmail.com>
2025-04-08 19:33:36 -0400

    Fix nav menu current page highlight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,21 @@
 module ApplicationHelper
-  def nav_link_class(is_active)
-    if is_active
+  def nav_link_class(path)
+    if active_path?(path)
       "rounded-md px-3 py-2 text-sm font-semibold bg-gray-900 text-white ring-1 ring-inset ring-white-700/10"
     else
       "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-400 hover:text-black"
     end
+  end
+
+  private
+
+  def active_path?(path)
+    return true if current_page?(path)
+
+    path.split("/").second == active_base_path
+  end
+
+  def active_base_path
+    request.path.split("/").second
   end
 end
