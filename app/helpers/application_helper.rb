109227a4d5a2c34ff4796db4ce140efc006c98a0
gangelo <web.gma@gmail.com>
2025-04-18 08:20:10 -0400

    Admin and Generate menu work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,28 +1,11 @@
 module ApplicationHelper
   def nav_link_class(path)
+    css_class = "rounded-md px-3 block lg:inline-block py-2 text-sm"
     if active_path?(path)
-      "rounded-md px-3 block lg:inline-block py-2 text-sm font-semibold bg-gray-900 text-white ring-1 ring-inset ring-white-700/10"
+      "#{css_class} font-semibold text-white ring-1 ring-inset ring-white-700/10"
     else
-      "rounded-md px-3 block lg:inline-block py-2 text-sm font-medium text-gray-300 hover:bg-gray-400 hover:text-black"
+      "#{css_class} font-medium text-gray-300 hover:bg-gray-400 hover:text-black"
     end
-  end
-
-  def active_path?(path)
-    if path.is_a?(Array)
-      path.any? { current_page?(it) }
-    else
-      current_page?(path)
-    end
-  end
-
-  def admin_active_paths
-    [
-      foods_path,
-      food_qualifiers_path,
-      health_conditions_path,
-      health_goals_path,
-      exports_path
-    ]
   end
 
   def titleize(model_object)
@@ -31,12 +14,18 @@ module ApplicationHelper
 
   private
 
+  # This method will return true if path matches the current_page exactly, or if
+  # path is a CRUD path (e.g. /foods/1/edit, /foods/1, /foods/new) and matches
+  # the base path of the current path (e.g. /foods, /food_qualifiers, etc.).
   def active_path?(path)
     return true if current_page?(path)
 
     path.split("/").second == active_base_path
   end
 
+  # This will returns the base path for the current request path. For example:
+  # if the request path was /foods/1/edit, /foods/1, /foods/new, etc., "foods"
+  # would be returned.
   def active_base_path
     request.path.split("/").second
   end
