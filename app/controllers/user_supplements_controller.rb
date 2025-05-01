7dc5fb036647feb32b8acc8279559ed8bf9111b8
gangelo <web.gma@gmail.com>
2025-05-01 17:17:07 -0400

    Fix user supplement ui AI bugs argh!

--- app/controllers/user_supplements_controller.rb
+++ app/controllers/user_supplements_controller.rb
@@ -25,7 +25,7 @@ class UserSupplementsController < ApplicationController
         format.turbo_stream do
           flash.now[:notice] = "Supplement was successfully added."
           render turbo_stream: [
-            turbo_stream.update("main_content", partial: "user_supplements/list/list", locals: { user_supplements: current_user.user_supplements.ordered }),
+            turbo_stream.update("main_content", partial: "form", locals: { user_supplement: @user_supplement }),
             turbo_stream.update("flash_messages", partial: "shared/flash_messages")
           ]
         end
@@ -43,15 +43,15 @@ class UserSupplementsController < ApplicationController
     # doesn't handle the removal of has_many through associations properly
     health_condition_ids = params[:user_supplement][:health_condition_ids]&.reject(&:blank?) || []
     health_goal_ids = params[:user_supplement][:health_goal_ids]&.reject(&:blank?) || []
-    
+
     respond_to do |format|
       if @user_supplement.update(user_supplement_params)
         # Associate health conditions and goals - always call these to handle checkbox unchecking
         update_health_conditions(health_condition_ids)
         update_health_goals(health_goal_ids)
-        
+
         @user_supplement.reload
-        
+
         format.html { redirect_to edit_user_supplement_path(@user_supplement), notice: "Supplement was successfully updated." }
         format.turbo_stream do
           flash.now[:notice] = "Supplement was successfully updated."
@@ -144,39 +144,39 @@ class UserSupplementsController < ApplicationController
 
   def user_supplement_params
     params.require(:user_supplement).permit(
-      :user_supplement_name, 
-      :form, 
-      :frequency, 
-      :dosage, 
-      :dosage_unit, 
+      :user_supplement_name,
+      :form,
+      :frequency,
+      :dosage,
+      :dosage_unit,
       :manufacturer,
       :notes,
-      health_condition_ids: [], 
+      health_condition_ids: [],
       health_goal_ids: [],
-      supplement_components_attributes: [:id, :supplement_component_name, :amount, :unit, :_destroy],
-      supplement_health_conditions_attributes: [:id, :health_condition_id, :_destroy],
-      supplement_health_goals_attributes: [:id, :health_goal_id, :_destroy]
+      supplement_components_attributes: [ :id, :supplement_component_name, :amount, :unit, :_destroy ],
+      supplement_health_conditions_attributes: [ :id, :health_condition_id, :_destroy ],
+      supplement_health_goals_attributes: [ :id, :health_goal_id, :_destroy ]
     )
   end
 
   def component_params
     params.require(:supplement_component).permit(:supplement_component_name, :amount, :unit)
   end
-  
+
   def update_health_conditions(health_condition_ids)
     # Clear existing associations
     @user_supplement.supplement_health_conditions.destroy_all
-    
+
     # Add new associations based on checkboxes
     health_condition_ids.each do |hc_id|
       @user_supplement.supplement_health_conditions.create(health_condition_id: hc_id)
     end
   end
-  
+
   def update_health_goals(health_goal_ids)
     # Clear existing associations
     @user_supplement.supplement_health_goals.destroy_all
-    
+
     # Add new associations based on checkboxes
     health_goal_ids.each do |hg_id|
       @user_supplement.supplement_health_goals.create(health_goal_id: hg_id)
