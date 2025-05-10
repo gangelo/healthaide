91cfb0169d19d980eb18a0d02ec96fd561a3cda8
gangelo <web.gma@gmail.com>
2025-05-10 15:55:06 -0400

    Remove favorite from user foods

--- db/schema.rb
+++ db/schema.rb
@@ -78,12 +78,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_02_010831) do
     t.integer "user_id", null: false
     t.integer "food_id", null: false
     t.boolean "available", default: true, null: false
-    t.boolean "favorite", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["food_id"], name: "index_user_foods_on_food_id"
     t.index ["user_id", "available"], name: "index_user_foods_on_user_id_and_available"
-    t.index ["user_id", "favorite"], name: "index_user_foods_on_user_id_and_favorite"
     t.index ["user_id", "food_id"], name: "index_user_foods_on_user_id_and_food_id", unique: true
     t.index ["user_id"], name: "index_user_foods_on_user_id"
   end
