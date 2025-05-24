9599e63aaa2f752b701ec63112493eb08b0371c1
gangelo <web.gma@gmail.com>
2025-05-23 20:08:05 -0400

    Add user dashboard to home view

--- spec/system/home_controller_spec.rb
+++ spec/system/home_controller_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe "Home" do
   context "when the user visits the home page" do
     it "displays the home page" do
       visit root_path
-      expect(page).to have_content("Home")
+      expect(page).to have_content("Welcome to HealthAIde")
     end
   end
 end
