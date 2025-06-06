91387f982150041311ecb118794b12e3c7507227
gangelo <web.gma@gmail.com>
2025-06-06 15:34:47 -0400

    Medications search service and specs

--- spec/support/init/webmock.rb
+++ spec/support/init/webmock.rb
@@ -1,12 +1,3 @@
 require "webmock/rspec"
 
-RSpec.configure do |config|
-  config.before(:each, type: :service) do
-    WebMock.enable!
-  end
-
-  config.after(:each, type: :service) do
-    WebMock.reset!
-    WebMock.disable!
-  end
-end
+WebMock.disable_net_connect!(allow_localhost: true)
