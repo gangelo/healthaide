1ac8a38c4c24be8436831f1eec1d5d603a555e2c
gangelo <web.gma@gmail.com>
2025-06-07 10:08:22 -0400

    Remove unused code

--- app/models/user_food.rb
+++ app/models/user_food.rb
@@ -8,19 +8,6 @@ class UserFood < ApplicationRecord
 
   scope :ordered, -> { includes(:food).order("foods.food_name") }
 
-  # Safely gets an attribute value from the attributes hash
-  # @param attribute_name [String, Symbol] name of the attribute to retrieve
-  # @param default [Object] value to return if attribute doesn't exist
-  # @return [Object] the attribute value or default if not found
-  def attribute_value(attribute_name, default = nil)
-    begin
-      attributes.fetch(attribute_name.to_s, default)
-    rescue StandardError => e
-      Rails.logger.error("Error retrieving attribute '#{attribute_name}': #{e.message}")
-      default
-    end
-  end
-
   def to_export_hash
     {
     user_food: attributes.symbolize_keys.tap do |hash|
